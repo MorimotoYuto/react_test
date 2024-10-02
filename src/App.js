9948a4e96bd0254c587f6e4237f318136898ef47
もり <morimoto_yuto@outlook.jp>
2024-10-02 04:50:39 +0000

    terminalからinstallしたらうまくいきました

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./styles.css";
 import * as React from "react";
-import { Button } from "@mui/material";
+import Button from "@mui/material/Button";
 export default function App() {
   return (
     <div className="App">
       <h1>Hello React!!!!</h1>
       <h2>Start editing to see some magic happen!</h2>
       <h3>codesandboxに慣れません</h3>
+      <Button>text</Button>
+      <Button variant="contained">contained</Button>
+      <Button variant="outlined">outlined</Button>
     </div>
   );
 }
