c642e58b88fb43d7a323d45e174ffe2f2bd1438c
もり <morimoto_yuto@outlook.jp>
2024-10-02 03:10:38 +0000

    App

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
     <div className="App">
       <h1>Hello React!!!!</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <h3>codesandboxに慣れません</h3>
     </div>
   );
 }
