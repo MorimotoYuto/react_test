f200d300a401e0cca906651dc80e42084409f573
もり <morimoto_yuto@outlook.jp>
2024-10-21 01:54:41 +0000

    add image

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import "./styles.css";
 import * as React from "react";
 import { Button } from "@mui/material";
+import CustomCard from "./mui/Card"; // Card.js をインポート
 export default function App() {
   return (
     <div className="App">
       <h1>Hello React!!!!</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <h3>codesandboxに慣れません</h3>
+      <h3>MUI難しい</h3>
       <Button>text</Button>
       <Button variant="contained">contained</Button>
       <Button variant="outlined">outlined</Button>
+      <CustomCard />
     </div>
   );
 }
---
+++ src/mui/Card.js
@@ -0,0 +1,29 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+
+const CustomCard = () => {
+  return (
+    <Card sx={{ maxWidth: 500 }}>
+      <CardMedia
+        component="img"
+        height="300"
+        image="/images/test.png" // ここに表示したい画像のURLを入れる
+        alt="Placeholder Image"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Card Title
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          This is an example of a card component using Material-UI with an
+          image.
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default CustomCard;
